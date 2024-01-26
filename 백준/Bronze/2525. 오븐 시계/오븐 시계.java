8dcde9b3a6d89bd7b03443eb6dadc7c33e05e189
hyeondo01 <156043136+hyeondo01@users.noreply.github.com>
2024-01-26 18:02:57 +0900

    [Bronze III] Title: 오븐 시계, Time: 236 ms, Memory: 18416 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if ((B + C) >= 60)
+        {
+            A = A + ((B + C) / 60);
+            B = (B + C) % 60;
+            if (A >= 24)
+            {
+                A = A - 24;
+            }
+        }
+        else B = B + C;
+
+        System.out.print(A + " " + B);
+    }
+}
