1ca87b0e494c8b6f30d87194b72b5fcc9a993a52
hyeondo01 <156043136+hyeondo01@users.noreply.github.com>
2024-01-30 18:11:37 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 14872 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader; // 입력
+import java.io.BufferedWriter; // 출력
+import java.io.IOException; // 예외처리
+import java.util.StringTokenizer; // 문자열 분리
+import java.io.InputStreamReader; // 입력 스트림
+import java.io.OutputStreamWriter; // 출력 스트림
+
+class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
