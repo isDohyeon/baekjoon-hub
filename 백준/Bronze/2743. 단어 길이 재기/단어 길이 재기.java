bb42ab2d56bb617dae1caee22484e139cc9df02a
hyeondo01 <156043136+hyeondo01@users.noreply.github.com>
2024-02-06 09:35:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader; // 입력
+import java.io.BufferedWriter; // 출력
+import java.io.IOException; // 예외처리
+import java.util.Arrays;
+import java.util.StringTokenizer; // 문자열 분리
+import java.io.InputStreamReader; // 입력 스트림
+import java.io.OutputStreamWriter; // 출력 스트림
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.length());
+    }
+}
