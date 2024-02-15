ccb7abcada33074cbbcad865f36c2b9129feca22
hyeondo01 <156043136+hyeondo01@users.noreply.github.com>
2024-02-15 21:55:12 +0900

    [Bronze V] Title: 새싹, Time: 128 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze V/25083. 새싹/새싹.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader; // 입력
+import java.io.BufferedWriter; // 출력
+import java.io.IOException; // 예외처리
+import java.util.Arrays;
+import java.util.StringTokenizer; // 문자열 분리
+import java.io.InputStreamReader; // 입력 스트림
+import java.io.OutputStreamWriter; // 출력 스트림
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        String s = "         ,r'\"7\n" + 	
+                "r`-_   ,'  ,/\n" + 		
+                " \\. \". L_r'\n" + 		
+                "   `~\\/\n" + 			
+                "      |\n" + 			
+                "      |";
+
+        System.out.print(s);
+    }
+}
