383a7ac08c2c5faeb2dfdacd6cc1f65a6df031e6
hyeondo01 <156043136+hyeondo01@users.noreply.github.com>
2024-02-16 14:02:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 548 ms, Memory: 24632 KB -BaekjoonHub

--- 백준/Bronze III/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze III/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -9,20 +9,19 @@ import java.util.Scanner;
 
 class Main
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for (int i = 1; i <= 2 * n - 1; i++)
         {
-            for (int j = 0; j < n-i; j++)
-            {
-                System.out.print(" ");
-            }
-
             if (i <= n)
             {
+                for (int j = 0; j < n-i; j++)
+                {
+                    System.out.print(" ");
+                }
                 for (int k = 0; k < 2 * i - 1; k++)
                 {
                     System.out.print("*");
