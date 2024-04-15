3bffe82d151c87bec3cd08626f9522ef88fe6491
hyeondo <156043136+hyeondo01@users.noreply.github.com>
2024-04-15 16:41:20 +0900

    [Silver II] Title: A → B, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int count = 1;
+        while (A != B)
+        {
+            if (A > B)
+            {
+                count = -1;
+                break;
+            }
+            if (B % 2 == 0)
+            {
+                B /= 2;
+            }
+            else if (B % 10 == 1)
+            {
+                B = (B - 1) / 10;
+            }
+            else
+            {
+                count = -1;
+                break;
+            }
+            count++;
+        }
+        System.out.print(count);
+    }
+}
