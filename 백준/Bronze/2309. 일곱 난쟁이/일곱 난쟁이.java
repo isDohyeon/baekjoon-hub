3185c9d28302bf662eff4add2ffb010bdf33fd2e
Dohyeon Park <eun041212@gmail.com>
2024-05-04 23:00:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -9,11 +9,14 @@ public class Main {
     public static final int LENGTH = 9;
 
      public static int[] solution(int[] tall, int sum) {
-         int[] answer = new int[LENGTH -2];
+         int[] answer = new int[LENGTH-2];
+         
          outerLoop:
+         // 난쟁이 두명씩 검사
          for (int i = 0; i < LENGTH -1; i++) {
             for (int j = i+1; j < LENGTH; j++) {
                 if ((sum - (tall[i] + tall[j])) == 100) {
+                    // 찾은 난쟁이 두명 빼고 정답 배열에 저장
                     int index = 0;
                     for (int k = 0; k < LENGTH; k++) {
                         if (k != i && k != j) {
@@ -21,6 +24,7 @@ public class Main {
                             index++;
                         }
                     }
+                    // 중첩 반복문 바로 탈출
                     break outerLoop;
                 }
             }
@@ -29,24 +33,26 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+         // 버퍼를 사용하여 입력받는 객체
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] tall = new int[LENGTH];
 
         int sum = 0;
+        // 난쟁이 키 입력받고 배열에 저장 후 더함
         for (int i = 0; i < LENGTH; i++) {
             tall[i] = Integer.parseInt(br.readLine());
             sum += tall[i];
         }
+        // 입력받은 난쟁이 키 정렬
         Arrays.sort(tall);
-
+        
         int[] result = solution(tall, sum);
-
+        
+        // 결과 출력
         for (int i = 0; i < result.length; i++) {
-            if (result[i] != 0) {
                 System.out.println(result[i]);
             }
-        }
-
+        
     }
 }
