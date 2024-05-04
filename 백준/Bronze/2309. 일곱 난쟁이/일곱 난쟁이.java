d2c31924152085d191b673f53b8c43b9e2191af2
Dohyeon Park <eun041212@gmail.com>
2024-05-04 21:54:30 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,36 +1,54 @@
-import java.util.*;
+//package CMD_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc =new Scanner(System.in);
-		int arr[] = new int[9];
-		int sum = 0;
-		int fake1 = 0, fake2=0;
-		
-		for(int i=0; i<arr.length; i++){
-			arr[i] = sc.nextInt(); 
-			sum += arr[i]; 
-		}
-		
-		Arrays.sort(arr); 
-		
-		for(int i=0; i<arr.length-1; i++){
-			for(int j=i+1; j<arr.length; j++){
-				if(sum - arr[i]-arr[j] == 100){
-					fake1 = i;
-					fake2 = j;
-					break;
-				}
-			}
-		}
+    public static final int NUM = 9;
 
-		
-		for(int i = 0; i<arr.length; i++) { 
-			if(i == fake1 || i == fake2)   
-				continue;
-			System.out.println(arr[i]);
-		}
-		sc.close();
-	}
+     public static int[] solution(int[] tall, int sum, int spyA, int spyB) {
+        for (int i = 0; i < NUM-1; i++) {
+            for (int j = i+1; j < NUM; j++) {
+                if ((sum - (tall[i] + tall[j])) == 100) {
+                    spyA = i;
+                    spyB = j;
+                    break;
+                }
+            }
+        }
+
+        int[] answer = new int[NUM];
+        for (int i = 0; i < NUM; i++) {
+            if (i == spyA || i == spyB) {
+                continue;
+            }
+            answer[i] = tall[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] tall = new int[NUM];
+        int spyA = 0;
+        int spyB = 0;
+        int sum = 0;
+        for (int i = 0; i < NUM; i++) {
+            tall[i] = Integer.parseInt(br.readLine());
+            sum += tall[i];
+        }
+        Arrays.sort(tall);
+
+        int[] result = solution(tall, sum, spyA, spyB);
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != 0) {
+                System.out.println(result[i]);
+            }
+        }
+
+    }
 }
