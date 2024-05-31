be00d40bbceb4a798397a1ca71b0be1481138192
Dohyeon Park <eun041212@gmail.com>
2024-05-31 13:41:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 216 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,71 +1,70 @@
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0}; // Directions for moving up, down, left, right
+    static int[] dy = {0, 0, -1, 1}; // Directions for moving up, down, left, right
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] matrix = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            String str = sc.next();
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = str.charAt(j) - '0';
+        Scanner scanner = new Scanner(System.in);
+        
+        N = scanner.nextInt();
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        
+        for (int i = 0; i < N; i++) {
+            String line = scanner.next();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        List<Integer> complexes = findComplex(matrix);
-        System.out.println(complexes.size());
-        for (int complex : complexes) {
-            System.out.println(complex);
-        }
-    }
-
-    public static List<Integer> findComplex(int[][] matrix) {
-        int n = matrix.length;
-        boolean[][] visited = new boolean[n][n];
-        List<Integer> complexes = new ArrayList<>();
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1 && !visited[i][j]) {
-                    count++;
-                    int complexCount = 0;
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j});
-                    visited[i][j] = true;
-
-                    while (!queue.isEmpty()) {
-                        int[] curr = queue.poll();
-                        complexCount++;
-
-                        int x = curr[0];
-                        int y = curr[1];
-
-                        if (x > 0 && matrix[x - 1][y] == 1 && !visited[x - 1][y]) {
-                            queue.offer(new int[]{x - 1, y});
-                            visited[x - 1][y] = true;
-                        }
-
-                        if (x < n - 1 && matrix[x + 1][y] == 1 && !visited[x + 1][y]) {
-                            queue.offer(new int[]{x + 1, y});
-                            visited[x + 1][y] = true;
-                        }
-
-                        if (y > 0 && matrix[x][y - 1] == 1 && !visited[x][y - 1]) {
-                            queue.offer(new int[]{x, y - 1});
-                            visited[x][y - 1] = true;
-                        }
-
-                        if (y < n - 1 && matrix[x][y + 1] == 1 && !visited[x][y + 1]) {
-                            queue.offer(new int[]{x, y + 1});
-                            visited[x][y + 1] = true;
-                        }
-                    }
-                    complexes.add(complexCount);
+        List<Integer> clusterSizes = new ArrayList<>();
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int clusterSize = dfs(i, j);
+                    clusterSizes.add(clusterSize);
                 }
             }
         }
-        Collections.sort(complexes);
-        return complexes;
+
+        Collections.sort(clusterSizes);
+        
+        System.out.println(clusterSizes.size());
+        for (int size : clusterSizes) {
+            System.out.println(size);
+        }
+
+        scanner.close();
+    }
+
+    private static int dfs(int x, int y) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{x, y});
+        visited[x][y] = true;
+        int count = 0;
+
+        while (!stack.isEmpty()) {
+            int[] current = stack.pop();
+            int cx = current[0];
+            int cy = current[1];
+            count++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    stack.push(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+        
+        return count;
     }
 }
