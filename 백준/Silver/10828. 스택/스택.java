af0ca73d26ebedf667f96ce9f7cf26286f5413c5
hyeondo <eun041212@gmail.com>
2024-07-03 16:49:07 +0900

    [Silver IV] Title: 스택, Time: 304 ms, Memory: 20524 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,46 +1,46 @@
 //package implemention;
-
-import java.util.*;
-import java.io.*;
+// java 코딩 컨벤션 적용
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
+    List<Integer> answer = new ArrayList<>();
 
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            String[] word = input.split(" ");
-            switch (word[0]) {
-
-                case "push":
-                stack.push(Integer.parseInt(word[1]));
-                break;
-                
-                case  "pop":
-                answer.add(stack.isEmpty() ? -1 : stack.pop());
-                break;
-
-                case "size":
-                answer.add(stack.isEmpty() ? 0 : stack.size());
-                break;
-
-                case "empty":
-                answer.add(stack.isEmpty() ? 1 : 0);
-                break;
-
-                case "top":
-                answer.add(stack.isEmpty() ? -1 : stack.peek());
-                break;
-            }
-        }
-        for (int output : answer) {
-            System.out.println(output);
-        }
-        
+    for (int i = 0; i < n; i++) {
+      String input = br.readLine();
+      String[] word = input.split(" ");
+      switch (word[0]) {
+        case "push":
+          stack.push(Integer.parseInt(word[1]));
+          break;
+        case "pop":
+          answer.add(stack.isEmpty() ? -1 : stack.pop());
+          break;
+        case "size":
+          answer.add(stack.size());
+          break;
+        case "empty":
+          answer.add(stack.isEmpty() ? 1 : 0);
+          break;
+        case "top":
+          answer.add(stack.isEmpty() ? -1 : stack.peek());
+          break;
+        default:
+          throw new IllegalArgumentException("Unknown command: " + word[0]);
+      }
     }
+
+    for (int output : answer) {
+      System.out.println(output);
+    }
+  }
 }
