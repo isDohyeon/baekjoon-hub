d7b03ac74964bdeac7fb9506b6cb9099f99151d3
hyeondo <eun041212@gmail.com>
2024-07-08 23:40:25 +0900

    [Bronze V] Title: N 찍기, Time: 124 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -4,47 +4,33 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 
 public class Main {
+    // 성능 개선 실험
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
         
-        BufferedOutputStream bos = new BufferedOutputStream(System.out, 32768);
+        BufferedOutputStream bos = new BufferedOutputStream(System.out, 8192);
 
-        byte[] buffer = new byte[588895]; // Pre-allocate buffer
+        byte[] buffer = new byte[n * 11];
         int index = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (i < 10) buffer[index++] = (byte)(i + '0');
-            else if (i < 100) {
-                buffer[index++] = (byte)(i / 10 + '0');
-                buffer[index++] = (byte)(i % 10 + '0');
+            int num = i;
+            int digits = 0;
+
+            while (num > 0) {
+                num /= 10;
+                digits++;
             }
-            else if (i < 1000) {
-                buffer[index++] = (byte)(i / 100 + '0');
-                buffer[index++] = (byte)((i / 10) % 10 + '0');
-                buffer[index++] = (byte)(i % 10 + '0');
+
+            num = i;
+            int start = index + digits - 1;
+            while (num > 0) {
+                buffer[start--] = (byte) ('0' + num % 10);
+                num /= 10;
             }
-            else if (i < 10000) {
-                buffer[index++] = (byte)(i / 1000 + '0');
-                buffer[index++] = (byte)((i / 100) % 10 + '0');
-                buffer[index++] = (byte)((i / 10) % 10 + '0');
-                buffer[index++] = (byte)(i % 10 + '0');
-            }
-            else if (i < 100000) {
-                buffer[index++] = (byte)(i / 10000 + '0');
-                buffer[index++] = (byte)((i / 1000) % 10 + '0');
-                buffer[index++] = (byte)((i / 100) % 10 + '0');
-                buffer[index++] = (byte)((i / 10) % 10 + '0');
-                buffer[index++] = (byte)(i % 10 + '0');
-            }
-            else {
-                buffer[index++] = '1';
-                buffer[index++] = '0';
-                buffer[index++] = '0';
-                buffer[index++] = '0';
-                buffer[index++] = '0';
-                buffer[index++] = '0';
-            }
+
+            index += digits;
             buffer[index++] = '\n';
         }
 
