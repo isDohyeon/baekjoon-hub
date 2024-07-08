7a31ce21ff4d429552ba80dca0122db3f827249b
hyeondo <eun041212@gmail.com>
2024-07-08 20:35:11 +0900

    [Bronze V] Title: N 찍기, Time: 128 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -4,37 +4,44 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 
 public class Main {
-    // 성능 개선 실험
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
-        
-        BufferedOutputStream bos = new BufferedOutputStream(System.out, 8192);
+        br.close();
 
-        byte[] buffer = new byte[n * 11];
+        BufferedOutputStream bos = new BufferedOutputStream(System.out, 8192);
+        // 버퍼 크기 예측: n * 6 (평균 5자리 숫자 + 줄바꿈 문자) -> 충분히 큰 버퍼
+        byte[] buffer = new byte[n * 6];
         int index = 0;
 
         for (int i = 1; i <= n; i++) {
             int num = i;
-            int digits = 0;
+            int start = index;
 
-            while (num > 0) {
-                num /= 10;
-                digits++;
+            if (num < 10) {
+                buffer[index++] = (byte) ('0' + num);
+            } else {
+                while (num > 0) {
+                    buffer[index++] = (byte) ('0' + num % 10);
+                    num /= 10;
+                }
+                reverse(buffer, start, index - 1);
             }
-
-            num = i;
-            int start = index + digits - 1;
-            while (num > 0) {
-                buffer[start--] = (byte) ('0' + num % 10);
-                num /= 10;
-            }
-
-            index += digits;
             buffer[index++] = '\n';
         }
 
         bos.write(buffer, 0, index);
         bos.flush();
+        bos.close();
+    }
+
+    private static void reverse(byte[] buffer, int start, int end) {
+        while (start < end) {
+            byte temp = buffer[start];
+            buffer[start] = buffer[end];
+            buffer[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
