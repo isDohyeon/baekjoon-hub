a73c1eec3ae61f86a21b432a72e3773b2fb51711
hyeondo <eun041212@gmail.com>
2024-07-08 14:44:36 +0900

    [Bronze V] Title: N 찍기, Time: 288 ms, Memory: 21116 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,15 +1,16 @@
 //package implemention;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
-    // Scanner 입력, BufferedWriter 출력
+    // BufferedReader 입력, BufferedWriter 출력
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 1; i <= n; i++) {
