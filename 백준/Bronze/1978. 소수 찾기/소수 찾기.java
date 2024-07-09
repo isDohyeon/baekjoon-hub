2406ea5c3f1993b09287aa74e86b0da19a623280
hyeondo <eun041212@gmail.com>
2024-07-10 08:02:40 +0900

    [Bronze II] Title: 소수 찾기, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,27 @@
+//package implemention;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = n;
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            if (num == 1) {
+                count--;
+                continue;
+            }
+            for (int j = 2; j < num; j++) {
+                if (num % j == 0) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
