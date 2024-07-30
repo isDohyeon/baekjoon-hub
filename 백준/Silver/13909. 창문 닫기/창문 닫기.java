d9651e965fd5e109684501d6e2b23ef94ffb743a
hyeondo <eun041212@gmail.com>
2024-07-31 00:35:44 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,14 @@
+//package algorithm.math;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print((int) Math.sqrt(n));
+    }
+}
