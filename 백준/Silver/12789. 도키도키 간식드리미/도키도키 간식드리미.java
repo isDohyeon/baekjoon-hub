36c6649a6ed9497b0a99fce9bf068f53ba412d35
hyeondo <eun041212@gmail.com>
2024-07-31 21:04:25 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 132 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,40 @@
+//package algorithm.datastructure;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int index = 1;
+
+        for (int i = 0; i < n; i++) {
+            int current = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek() == index) {
+                stack.pop();
+                index++;
+            }
+
+            if (current == index) {
+                index++;
+            } else {
+                stack.push(current);
+            }
+        }
+
+        while (!stack.isEmpty() && stack.peek() == index) {
+            stack.pop();
+            index++;
+        }
+
+        String result = index == n + 1 ? "Nice" : "Sad";
+        System.out.print(result);
+    }
+}
