696fd73f455a452d37ec08fc288e11d3fa99204d
hyeondo <eun041212@gmail.com>
2024-08-02 19:47:50 +0900

    [Silver I] Title: 절댓값 힙, Time: 340 ms, Memory: 25440 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,44 @@
+//package algorithm.datastructure;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> absHeap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int abs1 = Math.abs(o1);
+                int abs2 = Math.abs(o2);
+
+                if (abs1 == abs2) {
+                    return Integer.compare(o1, o2);
+                } else {
+                    return Integer.compare(abs1, abs2);
+                }
+            }
+        });
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) {
+                absHeap.add(x);
+            } else {
+                if (absHeap.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(absHeap.poll()).append('\n');
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+}
