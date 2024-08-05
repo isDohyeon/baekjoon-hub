cbdbdd43ccd822c3e1264b7c3d093913a6fac419
hyeondo <eun041212@gmail.com>
2024-08-05 19:47:49 +0900

    [Silver III] Title: 두 스티커, Time: 112 ms, Memory: 15220 KB -BaekjoonHub

---
+++ 백준/Silver/16937. 두 스티커/두 스티커.java
@@ -0,0 +1,71 @@
+//package algorithm.brute_force;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int height;
+    private static int width;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        height = Integer.parseInt(st.nextToken());
+        width = Integer.parseInt(st.nextToken());
+
+        int n = Integer.parseInt(br.readLine());
+        int[] r = new int[n];
+        int[] c = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            r[i] = Integer.parseInt(st.nextToken());
+            c[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int r1 = r[i];
+                int c1 = c[i];
+                int r2 = r[j];
+                int c2 = c[j];
+
+                maxArea = Math.max(maxArea, getMaxArea(r1, c1, r2, c2));
+            }
+        }
+
+        System.out.print(maxArea);
+    }
+
+    private static int getMaxArea(int r1, int c1, int r2, int c2) {
+        int maxArea = 0;
+
+        int[][] pair1 = {{r1, c1}, {c1, r1}};
+        int[][] pair2 = {{r2, c2}, {c2, r2}};
+
+        for (int[] p1 : pair1) {
+            for (int[] p2 : pair2) {
+                int pr1 = p1[0];
+                int pc1 = p1[1];
+                int pr2 = p2[0];
+                int pc2 = p2[1];
+
+                if (isValidStickers(pr1, pc1, pr2, pc2)) {
+                    maxArea = Math.max(maxArea, pr1 * pc1 + pr2 * pc2);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private static boolean isValidStickers(int r1, int c1, int r2, int c2) {
+        return (r1 + r2 <= height && Math.max(c1, c2) <= width) ||
+                (r1 + r2 <= width && Math.max(c1, c2) <= height) ||
+                (c1 + c2 <= height && Math.max(r1, r2) <= width) ||
+                (c1 + c2 <= width && Math.max(r1, r2) <= height);
+    }
+}
