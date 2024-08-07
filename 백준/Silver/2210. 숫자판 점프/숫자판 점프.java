e1b904e35477ff605fd1cb53796f3f237a0a0627
hyeondo <eun041212@gmail.com>
2024-08-07 19:18:55 +0900

    [Silver II] Title: 숫자판 점프, Time: 108 ms, Memory: 16216 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -6,16 +6,22 @@ import java.util.*;
 public class Main {
 
     private static final int N = 5;
-    private static int[][] board;
+    private static int[][] board = new int[N][N];
     private static int[] dx = {0, 1, 0, -1};
     private static int[] dy = {-1, 0, 1, 0};
     private static Set<Integer> numberSet = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
+        /**
+         * 1. 5x5 크기의 숫자판을 입력 받는다.
+         * 2. 모든 위치에서 시작하여 가능한 모든 6자리 수를 탐색한다.
+         * 3. 탐색한 6자리 수에 대해 중복되지 않도록 저장한다.
+         * 4. 저장된 수들의 개수를 출력한다.
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        board = new int[N][N];
 
+        // 1
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -23,16 +29,19 @@ public class Main {
             }
         }
 
+        // 2
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 findNumbers(i, j, 0, board[i][j]);
             }
         }
 
+        // 4
         System.out.print(numberSet.size());
     }
 
     private static void findNumbers(int x, int y, int depth, int number) {
+        // 3
         if (depth == 5) {
             numberSet.add(number);
             return;
@@ -43,7 +52,7 @@ public class Main {
             int ny = y + dy[i];
 
             if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                findNumbers(nx, ny, depth + 1, number * 10 + board[nx][ny]);
+                findNumbers(nx, ny, depth + 1, (number * 10) + board[nx][ny]);
             }
         }
     }
