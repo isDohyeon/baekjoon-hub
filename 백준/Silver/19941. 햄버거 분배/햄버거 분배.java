01400deb8e33d5cbab44c7641069e1602aab5559
hyeondo <eun041212@gmail.com>
2024-08-21 11:53:30 +0900

    [Silver III] Title: 햄버거 분배, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -1,3 +1,5 @@
+//package algorithm.greedy;
+
 import java.util.*;
 import java.io.*;
 
@@ -7,15 +9,15 @@ public class Main {
     private static int K;
 
     private static char[] arr;
-    private static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         arr = new char[N];
-        visited = new boolean[N];
+        
         String s = br.readLine();
         for (int i = 0; i < N; i++) {
             arr[i] = s.charAt(i);
@@ -29,20 +31,14 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             if (arr[i] == 'P') {
-                int index = Math.max(i - K, 0);
-
-                while (true) {
-                    if (index >= N || index > i + K) {
-                        break;
-                    }
-
-                    if (arr[index] == 'H') {
-                        arr[index] = 'X';
+                // 현재 사람 위치에서 왼쪽으로 K까지 햄버거를 탐색
+                for (int j = i - K; j <= i + K; j++) {
+                    if (j >= 0 && j < N && arr[j] == 'H') {
+                        // 매칭 가능한 햄버거를 발견했으면 매칭
+                        arr[j] = 'X';  // 햄버거를 사용했음을 표시
                         count++;
-                        break;
+                        break;  // 현재 사람은 햄버거를 먹었으므로 다음 사람으로 넘어감
                     }
-
-                    index++;
                 }
             }
         }
