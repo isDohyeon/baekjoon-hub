80e2329417eeebf71d4a4990e8abc60b0ea4bf0c
hyeondo <eun041212@gmail.com>
2024-08-24 04:14:22 +0900

    [Gold V] Title: 행복 유치원, Time: 500 ms, Memory: 54640 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -9,7 +9,7 @@ public class Main {
     private static int K;
 
     private static int[] tall;
-    private static List<TallGap> gap = new ArrayList<>();
+    private static int[] gap;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,12 +18,13 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         tall = new int[N];
+        gap = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             tall[i] = Integer.parseInt(st.nextToken());
             if (i > 0) {
-                gap.add(new TallGap(i, tall[i] - tall[i - 1]));
+                gap[i - 1] = tall[i] - tall[i - 1];
             }
         }
 
@@ -33,51 +34,12 @@ public class Main {
     private static int getMaxCost() {
         int cost = 0;
 
-        gap.sort(new Comparator<TallGap>() {
-            @Override
-            public int compare(TallGap o1, TallGap o2) {
-                if (o1.gap == o2.gap) {
-                    return o1.index - o2.index;
-                }
-                return o1.gap - o2.gap;
-            }
-        });
+        Arrays.sort(gap);
 
-//        List<Integer> indexes = new ArrayList<>();
-//        for (int i = 0; i < K - 1; i++) {
-//            indexes.add(gap.get(i).index);
-//        }
-//
-//        Collections.sort(indexes);
-//        for (TallGap tallGap : gap) {
-//            System.out.println("tallGap = " + tallGap.index + ", gap : " + tallGap.gap);
-//        }
-
-        int start = 0;
-//        for (Integer index : indexes) {
-//            cost += tall[index - 1] - tall[start];
-//            start = index;
-//        }
-        for (int i = (N - 1) - (K - 1); i < N - 1; i++) {
-            int index = gap.get(i).index;
-//            System.out.println("i = " + i);
-//            System.out.println("index = " + index);
-            cost += tall[index - 1] - tall[start];
-            start = index;
+        for (int i = 0; i <= N - K; i++) {
+            cost += gap[i];
         }
-
-        cost += tall[N - 1] - tall[start];
 
         return cost;
-    }
-
-    private static class TallGap {
-        int index;
-        int gap;
-
-        public TallGap(int index, int gap) {
-            this.index = index;
-            this.gap = gap;
-        }
     }
 }
