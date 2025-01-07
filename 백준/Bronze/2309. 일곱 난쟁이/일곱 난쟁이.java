6d79d501e32cafc91082aea583aaac7f0744be16
hyeondo <eun041212@gmail.com>
2025-01-07 22:27:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,58 +1,62 @@
-//package CMD_Algorithm;
+//package week01;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main {
-    public static final int LENGTH = 9;
 
-     public static int[] solution(int[] tall, int sum) {
-         int[] answer = new int[LENGTH-2];
-         
-         outerLoop:
-         // 난쟁이 두명씩 검사
-         for (int i = 0; i < LENGTH -1; i++) {
-            for (int j = i+1; j < LENGTH; j++) {
-                if ((sum - (tall[i] + tall[j])) == 100) {
-                    // 찾은 난쟁이 두명 빼고 정답 배열에 저장
-                    int index = 0;
-                    for (int k = 0; k < LENGTH; k++) {
-                        if (k != i && k != j) {
-                            answer[index] = tall[k];
-                            index++;
-                        }
+    private static final int N = 9;
+    private static int[] tall;
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            tall = new int[N];
+            int sum = 0;
+            // 1. 키 입력받으며 키의 총 합을 구함
+            for (int i = 0; i < N; i++) {
+                int length = Integer.parseInt(br.readLine());
+                tall[i] = length;
+                sum += length;
+            }
+            // 2. 키를 오름차순 정렬
+            Arrays.sort(tall);
+            // 3. 2명이 빠진 모든 키의 합를 검사
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = 1; j < N; j++) {
+                    if (findImposter(sum, i, j, bw)) {
+                        return;
                     }
-                    // 중첩 반복문 바로 탈출
-                    break outerLoop;
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return answer;
     }
 
-    public static void main(String[] args) throws IOException {
-         // 버퍼를 사용하여 입력받는 객체
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] tall = new int[LENGTH];
-
-        int sum = 0;
-        // 난쟁이 키 입력받고 배열에 저장 후 더함
-        for (int i = 0; i < LENGTH; i++) {
-            tall[i] = Integer.parseInt(br.readLine());
-            sum += tall[i];
+    private static boolean findImposter(int sum, int i, int j, BufferedWriter bw) throws IOException {
+        // 키의 총합 - 두 명의 키의 합 == 100이라면
+        // -> 진짜 난쟁이 7명을 찾은 것
+        if ((sum - (tall[i] + tall[j])) == 100) {
+            printDwarfs(i, j, bw);
+            return true;
         }
-        // 입력받은 난쟁이 키 정렬
-        Arrays.sort(tall);
-        
-        int[] result = solution(tall, sum);
-        
-        // 결과 출력
-        for (int i = 0; i < result.length; i++) {
-                System.out.println(result[i]);
+        return false;
+    }
+
+    private static void printDwarfs(int i, int j, BufferedWriter bw) throws IOException {
+        // 임포스터 난쟁이 두 명 빼고 모두 출력
+        for (int k = 0; k < N; k++) {
+            if (k == i || k == j) {
+                continue;
             }
-        
+            bw.write(String.valueOf(tall[k]));
+            bw.newLine();
+        }
+        bw.flush();
     }
 }
